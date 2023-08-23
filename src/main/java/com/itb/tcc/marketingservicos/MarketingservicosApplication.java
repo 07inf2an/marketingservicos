d0cea05e9ba68ae33a155c6f45cf5cd82041dbf0
Enzo Basile <rm87456@estudante.fieb.edu.br>
2023-08-23 18:41:42 -0300

    Criação do ambiente de desenvolvimento

---
+++ src/main/java/com/itb/tcc/marketingservicos/MarketingservicosApplication.java
@@ -0,0 +1,13 @@
+package com.itb.tcc.marketingservicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketingservicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketingservicosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itb/tcc/marketingservicos/MarketingservicosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itb.tcc.marketingservicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketingservicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
